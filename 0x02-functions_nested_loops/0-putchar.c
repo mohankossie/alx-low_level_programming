fed7545720e73debd509692019225cf4aa37daaa
mohankossie <mohankossie@gmail.com>
2023-02-21 07:47:51 -0800

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
-/**
- * main - print _putchar
- * written by mohankossie
- * Return: 0
- **/
 
+/**
+ * main -  prints _putchar
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	_putchar('_');
@@ -16,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
